4a3875d34dfb2885a79c7ae4f8587b127b020521
Brian Whitaker <43477669+bwwhitaker@users.noreply.github.com>
2025-05-21 13:59:28 -0700

    Add responsive CTA component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './Home.css';
+import adu1 from '../../assets/adu1.png';
+import adu2 from '../../assets/adu2.png';
+import adu3 from '../../assets/adu3.png';
+import nieghborhood from '../../assets/neighborhood.png';
 
 export default function Home() {
 	return (
 		<div className='body'>
 			<div>
-				<div className='home-cta'>Create Positive Change in Your Communities</div>
+				<div className='hero'>Create Positive Change in Your Communities</div>
+				<div className='calculator-cta-container'>
+					<div className='adu-image-container'>
+						<img src={adu1} className='adu1-image' alt='an example of an adu' />
+						<div className='stacked-image-container'>
+							<img src={adu2} className='adu2-image' alt='an example of an adu' />
+							<img src={adu3} className='adu3-image' alt='an example of an adu' />
+						</div>
+					</div>
+					<div className='calculator-cta-card'>
+						<h2>An Average ADU costs $150,000</h2>
+						<p>But what if you split the cost 6 ways?</p>
+						<p>Earn a 14% return on your share to break even in 7 years.</p>
+						<p>
+							Customize results based on region, unit size, bedrooms, and other amenities. Try our <a>calculator</a>{' '}
+							today.{' '}
+						</p>
+					</div>
+				</div>
+				{/* <div className='neighborhood-effort-cta-container'>
+					<h1>Team up with neighbors to build housing together.</h1>
+					<img src={nieghborhood} className='neighborhood-image' alt='neighborhood image' />
+				</div> */}
 			</div>
 		</div>
 	);
